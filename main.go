7d9dbdd52852b7498c90c21735df8721021ce371
BlackestWhite <pesaregoal@gmail.com>
2021-06-06 16:03:14 +0430

    update

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	// download("http://dl6.shirazsong.in/dl/music/99-11/Mehdi%20Jahani%20-%20Asemoone%20Mani.mp3")
+	http.HandleFunc("/api/v1/convert", convertRoute)
+	http.ListenAndServe(":80", nil)
+}
+
+func convertRoute(w http.ResponseWriter, r *http.Request) {
+	fileUrl := r.URL.Query().Get("file_url")
+	err := download(fileUrl)
+	if err != nil {
+		fmt.Fprintf(w, "error while downloading file")
+		return
+	}
+	convert()
 }
