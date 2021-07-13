08ccd8c4b485eaa9339169a0d92235763cf07186
BlackestWhite <pesaregoal@gmail.com>
2021-07-13 22:45:29 +0430

    interact with job channel

--- converter.go
+++ converter.go
@@ -4,13 +4,17 @@ import (
 	"os/exec"
 )
 
-func convert() error {
-	cmd := exec.Command("./ffmpeg", "-i", "inp.mp3", "-c:a", "libmp3lame", "-b:a", "320k", "-map", "0:0", "-f", "segment", "-segment_time", "10", "-segment_list", "outputlist.m3u8", "-segment_format", "mpegts", "output%03d.ts")
+func convertFile(jobID string) {
+	cmd := exec.Command("./ffmpeg", "-i", "./temp/inp.mp3", "-c:a", "libmp3lame", "-b:a", "320k", "-map", "0:0", "-f", "segment", "-segment_time", "10", "-segment_list", "outputlist.m3u8", "-segment_format", "mpegts", "output%03d.ts")
 	_, err := cmd.Output()
 
 	if err != nil {
-		return err
+		j := jobs[jobID]
+		passToChannel(&j, "failed converting")
+		killSig(&j)
+		return
 	}
 
-	return nil
+	j := jobs[jobID]
+	passToChannel(&j, "converted")
 }
