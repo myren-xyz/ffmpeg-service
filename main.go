b31f2a9e5bfb65071ea618093cb1b358e1e13862
BlackestWhite <pesaregoal@gmail.com>
2021-07-13 22:46:44 +0430

    update

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 var config Config
+var jobs map[string]Job
 
 func init() {
-
+	jobs = make(map[string]Job)
 	file, err := ioutil.ReadFile("./.config.json")
 	if err != nil {
 		fmt.Printf("error in reading json config file: %s\n", err)
@@ -24,29 +25,53 @@ func init() {
 
 func main() {
 	// download("http://dl6.shirazsong.in/dl/music/99-11/Mehdi%20Jahani%20-%20Asemoone%20Mani.mp3")
-	// http.HandleFunc("/api/v1/convert", convertRoute)
+	http.HandleFunc("/api/v1/convert", convertRoute)
 	http.HandleFunc("/api/v1/subscribe", subscribe)
-	// http.ListenAndServe(":80", nil)
-	// upload()
+	http.ListenAndServe(":8000", nil)
 }
 
 func convertRoute(w http.ResponseWriter, r *http.Request) {
 	fileUrl := r.URL.Query().Get("file_url")
 	// issuer should be passed when converting and uploading has been finished
+	// should return job id
 	_ = r.URL.Query().Get("issuer")
+	newJobID := generateSeq(6)
+	job := Job{
+		Status:  make(chan string, 1),
+		Notify:  make(chan string, 1),
+		KillSig: make(chan bool, 1),
+	}
 
-	err := download(fileUrl)
-	if err != nil {
-		fmt.Fprintf(w, "error while downloading file")
-		return
-	}
-	err = convert()
-	if err != nil {
-		fmt.Fprintf(w, "error while converting file")
-		return
-	}
+	go startAct(fileUrl, newJobID)
+
+	jobs[newJobID] = job
+	j := jobs[newJobID]
+	passToChannel(&j, "inq")
+	fmt.Fprintf(w, newJobID)
+
 }
 
 func subscribe(w http.ResponseWriter, r *http.Request) {
-	_ = r.URL.Query().Get("job_id")
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	jobID := r.URL.Query().Get("job_id")
+	// if jobs[jobID] == (Job{}) {
+	// 	// no jobs availavle with this jobID
+
+	// }
+	select {
+	case e := <-jobs[jobID].Notify:
+		mar, _ := json.Marshal(e)
+		fmt.Fprintf(w, "data: %s\n\n", string(mar))
+		fmt.Printf("%v\n", string(mar))
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
 }
