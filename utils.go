3f89a63586af54ccf5b2c5285b0b55ceacc2ae7f
BlackestWhite <pesaregoal@gmail.com>
2021-07-13 22:44:50 +0430

    kill signal channel & route acts for status

--- utils.go
+++ utils.go
@@ -16,5 +16,33 @@ func generateSeq(length int) string {
 }
 
 func passToChannel(job *Job, data string) {
-	go func() { job.Status <- &data }()
+	go func() {
+		job.Status <- data
+	}()
+	go func() {
+		job.Notify <- data
+	}()
+}
+
+func killSig(job *Job) {
+	go func() {
+		job.KillSig <- true
+	}()
+}
+
+func startAct(url string, jobID string) {
+	for {
+		select {
+		case status := <-jobs[jobID].Status:
+			if status == "inq" {
+				go download(url, jobID)
+			} else if status == "fetched" {
+				go convertFile(jobID)
+			} else if status == "converted" {
+				go upload(jobID)
+			}
+		case <-jobs[jobID].KillSig:
+			break
+		}
+	}
 }
